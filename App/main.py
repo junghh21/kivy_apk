7c43b00719c7c89af06488c8ca4d209a90f1cc53
junghh21 <junghh21@gmail.com>
2023-12-05 16:07:31 +0900

    Update main.py

--- App/main.py
+++ App/main.py
@@ -1,78 +1,52 @@
 from kivy.lang import Builder
+
 from kivymd.app import MDApp
 
-root_kv = """
-MDBoxLayout:
-    orientation: "vertical"
 
-    MDToolbar:
-        id: toolbar
-        title: "Test MDBottomNavigation"
-        md_bg_color: app.theme_cls.primary_color
-        left_action_items: [["menu", lambda x: None]]
-
-    MDBottomNavigation:
-        id: panel
-
-        MDBottomNavigationItem:
-            name: "files1"
-            text: "Python"
-            icon: "language-python"
-
-            BoxLayout:
-                orientation: "vertical"
-                size_hint_y: None
-                height: self.minimum_height
-                spacing: dp(10)
-                pos_hint: {"center_x": .5, "center_y": .5}
-
-                MDLabel:
-                    font_style: "Body1"
-                    theme_text_color: "Primary"
-                    text: "Toggle to set custom panel color"
-                    halign: "center"
-
-                MDSwitch:
-                    size_hint: None, None
-                    size: dp(36), dp(48)
-                    pos_hint: {"center_x": .5}
-                    on_active:
-                        panel.panel_color = \
-                        [0.2980392156862745, 0.2823529411764706, 0.32941176470588235, 1] \
-                        if self.active else app.theme_cls.bg_dark
-
-        MDBottomNavigationItem:
-            name: "files2"
-            text: "C++"
-            icon: "language-cpp"
-
-            MDLabel:
-                font_style: "Body1"
-                theme_text_color: "Primary"
-                text: "I programming of C++"
-                halign: "center"
-
-        MDBottomNavigationItem:
-            name: "files3"
-            text: "JS"
-            icon: "language-javascript"
-
-            MDLabel:
-                font_style: "Body1"
-                theme_text_color: "Primary"
-                text: "Oh god JS again"
-                halign: "center"
-"""
-
-
-class MainApp(MDApp):
-    def __init__(self, **kwargs):
-        self.title = "KivyMD Examples - Bottom Navigation"
-        super().__init__(**kwargs)
+class Test(MDApp):
 
     def build(self):
-        self.root = Builder.load_string(root_kv)
+        self.theme_cls.material_style = "M3"
+        self.theme_cls.theme_style = "Dark"
+        return Builder.load_string(
+            '''
+MDScreen:
+
+    MDBottomNavigation:
+        #panel_color: "#eeeaea"
+        selected_color_background: "orange"
+        text_color_active: "lightgrey"
+
+        MDBottomNavigationItem:
+            name: 'screen 1'
+            text: 'Mail'
+            icon: 'gmail'
+            badge_icon: "numeric-10"
+
+            MDLabel:
+                text: 'Mail'
+                halign: 'center'
+
+        MDBottomNavigationItem:
+            name: 'screen 2'
+            text: 'Twitter'
+            icon: 'twitter'
+            badge_icon: "numeric-5"
+
+            MDLabel:
+                text: 'Twitter'
+                halign: 'center'
+
+        MDBottomNavigationItem:
+            name: 'screen 3'
+            text: 'LinkedIN'
+            icon: 'linkedin'
+
+            MDLabel:
+                text: 'LinkedIN'
+                halign: 'center'
+'''
+        )
 
 
-if __name__ == "__main__":
-    MainApp().run()
+Test().run()
